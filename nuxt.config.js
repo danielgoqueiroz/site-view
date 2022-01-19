d5bebf55e8d4f462770a2256c0609b168f479cf1
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2022-01-19 18:21:43 -0300

    docs: deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,11 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    babel: {
+      compact: true,
+    },
+  },
 
   pwa: {
     icon: {
