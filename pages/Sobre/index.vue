ea507570621aa939cfe18ad0ceab8fc84684a76e
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2022-01-20 22:42:24 -0300

    feat: uso de nuxt content em Sobre

--- pages/Sobre/index.vue
+++ pages/Sobre/index.vue
@@ -12,6 +12,7 @@
           rounded="circle"
         />
       </b-col>
+
       <b-col cols="5">
         <transition name="fadeAnimation">
           <div class="info">
@@ -21,13 +22,13 @@
               :key="info.key"
             >
               <h1>Olá, eu sou<br />Daniel G. Queiroz</h1>
-              <h2 class="font-italic">o {{ info.type }}</h2>
-              <p>{{ info.content }}</p>
+              <nuxt-content :document="info" />
             </b-container>
             <p></p>
           </div>
         </transition>
       </b-col>
+
       <b-col align-self="center">
         <b-button-close @click="switchOption()">
           <b-icon-arrow-right-circle font-scale="2.5" />
@@ -43,28 +44,14 @@ export default {
   components: {
     SocialIcons,
   },
+  async mounted() {
+    const infos = await this.$content('about').fetch()
+    this.infos = infos
+  },
   data() {
     return {
+      infos: [],
       infoChosed: 1,
-      infos: [
-        {
-          type: 'Fotógrafo',
-          content: `Como primeira formação publicidade e propaganda (2005), 
-                e competência na produção visual em fotografia e vídeos, atuei por oito anos em jornal
-                diário, online e impresso, em Florianópolis SC, além de alguns anos com fotografia social. 
-                Ao todo foram 14 anos dedicados profissionalmente a produção audiovisual, 
-                tendo participado de alguns prêmios nacionais e regionais de jornalismo.
-                Atualmente atuo como desenvolvedor na Dígitro tecnologia.`,
-        },
-        {
-          type: 'Desenvolvedor',
-          content: `Após uma mudança de carreira em 2018, tenho atuado como desenvolvedor backend na Dígitro Tecnologia. 
-                Tenho atuado como  backend usando a linguegm Java. Além disso, já utilizei tenho usado VueJS no frontEnd em alguns projetos e
-                já trabalei com banco de dados dados relacionais, como Postgres, Oracle e MySql, e alguns NoSQL como MongoDB e Elasticsearch. 
-                Atualmente faço pós-graduação em ciência de dados na PUC-RS (online).
-                Anteriormente, possuo formação em publicidade e propaganda, tendo atuado na produção audiovisual por mais de 10 anos.`,
-        },
-      ],
       imageScale: '100%',
     }
   },
